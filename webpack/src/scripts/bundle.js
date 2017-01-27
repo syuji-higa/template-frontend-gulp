333c1c44bc9a814b04e94330dfe73ecd57b60849
syuji-higa <higa@trymore-inc.jp>
2017-01-27 14:27:14 +0900

    よく使うモジュールの import を変更

--- webpack/src/scripts/bundle.js
+++ webpack/src/scripts/bundle.js
@@ -1,15 +1,15 @@
-// import 'html5-polyfills/classList';
-// import 'html5-polyfills/dataset';
-// import 'fetch';
-// import 'rAF';  // https://gist.github.com/1579671.git
-// import 'polyfill/function';
-// import 'lodash';
-// import 'Velocity';
-// import 'Velocity.ui';
-// import 'velocity.easeplus';
-// import 'custom-event-polyfill';  // https://github.com/krambuhl/custom-event-polyfill.git
-// import 'html-domparser';  // https://gist.github.com/1129031.git
-import { createIncetance, createSingleIncetance } from 'view/incetance';
+import 'babel-polyfill';  // https://www.npmjs.com/package/babel-polyfill
+import 'html5-polyfills/classList';
+import 'html5-polyfills/dataset';
+import 'fetch-polyfill';  // https://www.npmjs.com/package/fetch-polyfill for ie8+
+import 'whatwg-fetch';  // https://www.npmjs.com/package/whatwg-fetch for ie10+
+import 'rAF';  // https://gist.github.com/1579671.git
+import 'custom-event-polyfill';  // https://www.npmjs.com/package/custom-event-polyfill
+import 'html-domparser';  // https://gist.github.com/1129031.git
+import 'velocity';  // https://www.npmjs.com/package/velocity-animate
+import 'velocity.ui';  // https://www.npmjs.com/package/velocity-animate
+import 'velocity.easeplus';  // https://github.com/syuji-higa/easeplus-velocity
+import { createIncetance, createSingleIncetance } from 'utility/incetance';
 import Sample from 'sample';
 
 window.addEventListener('DOMContentLoaded', () => {
